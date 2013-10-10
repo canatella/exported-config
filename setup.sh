f7c820f55d68db855ce84a5db0fd67c388845c29
Damien Merenne <dam@cosinux.org>
2013-10-10 16:33:43 +0200

    add logging messages

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 CDIR=$HOME/.exported-config
+
+echo cloning config directory to $CDIR
 git clone https://github.com/canatella/exported-config.git $CDIR
+
+echo cloning emacs config
 git clone https://github.com/canatella/exported-config.git $HOME/.emacs.d
 
+echo installing keyboard setup
 desktop-file-install --dir=$HOME/.config/autostart $CDIR/xkb/xkb-setup.desktop
