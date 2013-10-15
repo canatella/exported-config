bb09bda0e0ea70af635284b9e632094a3b7178a7
Damien Merenne <dam@cosinux.rg>
2013-10-15 11:22:19 +0200

    - add package install - fix emacs config install

--- setup.sh
+++ setup.sh
@@ -2,11 +2,14 @@
 
 CDIR=$HOME/.exported-config
 
+echo installing packages
+sudo apt-get install mc emacs24 emacs24-el
+
 echo cloning config directory to $CDIR
 git clone https://github.com/canatella/exported-config.git $CDIR
 
 echo cloning emacs config
-git clone https://github.com/canatella/exported-config.git $HOME/.emacs.d
+git clone https://github.com/canatella/emacscfg.git $HOME/.emacs.d
 
 echo installing keyboard setup
 desktop-file-install --dir=$HOME/.config/autostart $CDIR/xkb/xkb-setup.desktop
